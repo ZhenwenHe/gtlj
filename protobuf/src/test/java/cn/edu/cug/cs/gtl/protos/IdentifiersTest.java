526fa409f1ea4c3c045ca620dda4992faf6daff5
ZhenwenHe <zwhe@cug.edu.cn>
2019-10-07 15:05:10 +0800

    initial

---
+++ protobuf/src/test/java/cn/edu/cug/cs/gtl/protos/IdentifiersTest.java
@@ -0,0 +1,16 @@
+package cn.edu.cug.cs.gtl.protos;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class IdentifiersTest {
+
+    @Test
+    public void allTest() {
+        Identifier.Builder builder = Identifier.newBuilder();
+        builder.setData(12l);
+        Identifier identifier=builder.build();
+        assertEquals(12l,identifier.getData());
+    }
+}
